19ab410cf0973ba7f8feb55b8d76c9f1dce835e3
Soumya <soumyacool2012@gmail.com>
2018-08-16 02:40:00 +0530

    Added SCSS support

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,56 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import { Menu } from 'semantic-ui-react'
+import { Menu } from "semantic-ui-react";
 
-class Navbar extends Component 
-{
-    render() 
-    {
-        return (
-            <div>
-                <Menu>
-                    <Menu.Item 
-                        style={styles.logo_area} 
-                    >
-                        <img src='/assets/IIIT_Bhubaneswar_Logo.png' 
-                            style={styles.logo} 
-                        />
-                    </Menu.Item>
-                    
-                    <Menu.Item
-                        style={styles.heading_area} 
-                    >
-                        <div>
-                            Tech Scoiety - IIIT Bh
-                        </div>
-                    </Menu.Item>
-                </Menu>
-            </div>
-        );
-    }
+class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <Menu>
+          <Menu.Item style={styles.logo_area}>
+            <img src="/assets/IIIT_Bhubaneswar_Logo.png" style={styles.logo} />
+          </Menu.Item>
+
+          <Menu.Item style={styles.heading_area}>
+            <div>Tech Scoiety - IIIT Bh</div>
+          </Menu.Item>
+        </Menu>
+      </div>
+    );
+  }
 }
 
 const styles = {
-    logo_area: {
-        height: "10vh",
-        width: "10%",
-        padding: 0,
-        paddingTop: 5,
-        paddingBottom: 5
-    },
-    heading_area: {
-        height: "10vh",
-        width: "90%",
-        padding: 0,
-        display: 'flex',
-        justifyContent: 'center',
-        fontSize: 24
-    },
-    logo: {
-        height: "100%",
-        width: "100%",
-        objectFit: "contain"
-    }
-}
+  logo_area: {
+    height: "10vh",
+    width: "10%",
+    padding: 0,
+    paddingTop: 5,
+    paddingBottom: 5
+  },
+  heading_area: {
+    height: "10vh",
+    width: "90%",
+    padding: 0,
+    display: "flex",
+    justifyContent: "center",
+    fontSize: 24
+  },
+  logo: {
+    height: "100%",
+    width: "100%",
+    objectFit: "contain"
+  }
+};
 
 export default Navbar;
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,13 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import 'semantic-ui-css/semantic.min.css';
+import "semantic-ui-css/semantic.min.css";
 
-import Navbar from '../components/Navbar/index';
+import Navbar from "../components/Navbar/index";
 
-class App extends Component 
-{
-  render() 
-  {
+class App extends Component {
+  render() {
     return (
       <div>
         <Navbar />
