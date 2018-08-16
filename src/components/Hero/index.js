b7c5b1a5c09af16b4045e33a27c84f96cd97580f
sonu <sonusaikishan@gmail.com>
2018-08-16 22:53:19 +0530

    Added Typing Effect in Hero Component

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -4,10 +4,29 @@ import image from '../../images/background.png';
 
 import ImageCarousel from '../Carousel';
 
+import Typing from 'react-typing-animation';
+
 import './style.css';
 
-class Hero extends Component {
-    render() {
+class Hero extends Component 
+{
+    constructor(props)
+    {
+        super(props);
+
+        this.state = {
+            societies: [
+                "ACM IIIT Chapter",
+                "TARS",
+                "Mozilla Campus Club",
+                "P-Society",
+                "PWL"
+            ]
+        }
+    }
+
+    render() 
+    {
         return (
             <div className="hero"
                 style = {{
@@ -15,8 +34,42 @@ class Hero extends Component {
                 }}
             >
                 <div className="heading" >
-                    Tech Society <br />
-                    IIIT-Bhubaneswar
+                    <div>
+                        Tech Society <br />
+                        IIIT-Bhubaneswar
+                    </div>
+                    <br />
+                    <div className="societies" >
+                    <Typing loop={true}  speed={110}>
+
+                        <span>{this.state.societies[0]}</span>
+
+                        <Typing.Delay ms={1000} />
+
+                        <Typing.Backspace count={this.state.societies[0].length} />
+                        <span>{this.state.societies[1]}</span>
+
+                        <Typing.Delay ms={1500} />
+
+                        <Typing.Backspace count={this.state.societies[1].length} />
+                        <span>{this.state.societies[2]}</span>
+
+                        <Typing.Delay ms={1000} />
+
+                        <Typing.Backspace count={this.state.societies[2].length} />
+                        <span>{this.state.societies[3]}</span>
+
+                        <Typing.Delay ms={1000} />
+
+                        <Typing.Backspace count={this.state.societies[3].length} />
+                        <span>{this.state.societies[4]}</span>
+
+                        <Typing.Delay ms={1500} />
+
+                        <Typing.Backspace count={this.state.societies[4].length} />
+
+                    </Typing>
+                    </div>
                 </div>
 
                 <div className="carousel" >
