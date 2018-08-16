7a03aca04585d08adc90dfe1a5d9f87e475bd2c0
sonu <sonusaikishan@gmail.com>
2018-08-16 21:33:09 +0530

    Made the Hero Page

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 
+import image from '../../images/background.png';
+
+import ImageCarousel from '../Carousel';
+
+import './style.css';
+
 class Hero extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="hero"
+                style = {{
+                    backgroundImage: `url(${image})`
+                }}
+            >
+                <div className="heading" >
+                    Tech Society <br />
+                    IIIT-Bhubaneswar
+                </div>
+
+                <div className="carousel" >
+                    <ImageCarousel />
+                </div>
             </div>
         );
     }
