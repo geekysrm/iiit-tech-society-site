6ec9b287f00d7aaea9f0979c6b895805e8edc190
Soumya <soumyacool2012@gmail.com>
2018-08-17 16:46:27 +0530

    Testing contact form

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -3,5 +3,29 @@ import React from "react";
 import "./style.css";
 
 export default () => {
-  return <div />;
+  return (
+    <div>
+      <form name="contact" method="post">
+        <input type="hidden" name="form-name" value="contact" />
+        <p>
+          <label>
+            Your Name: <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Email: <input type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message: <textarea name="message" />
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+    </div>
+  );
 };
